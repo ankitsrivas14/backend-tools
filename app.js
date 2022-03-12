39d4d1b2ccab04b1acf51ae5730b6bb297bcbf06
Ankit Srivastava <ankitsrivas14@gmail.com>
2022-03-12 22:42:20 +0530

    added body parser

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express')();
+const bodyParser = require('body-parser');
 
+express.use(bodyParser.urlencoded());
 
-express.use((req,res,next) => {
-    console.log("In the middleware!");
-    next();
-})
-
-express.use((req,res,next) => {
-    console.log("In another middleware!");
-})
+express.post('/test',(req,res,next) => {
+    res.send('<h1>POST</h1>')
+});
+express.get('/test',(req,res,next) => {
+    res.send('<h1>GET</h1>')
+});
 
 
 express.listen(3000);
