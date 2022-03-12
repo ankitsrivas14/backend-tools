a07d0d6675e7e7aa3d36471577e768a6833841a8
Ankit Srivastava <ankitsrivas14@gmail.com>
2022-03-12 23:09:31 +0530

    added not found route

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ const shopRoutes = require('./routes/shop');
 
 express.use(bodyParser.urlencoded());
 
+//Routes
 express.use(adminRoutes);
 express.use(shopRoutes);
+
+
+//Not found router
+express.use('/', (req, res, next) => {
+    res.status(404).send("<h1>Page not found</h1>");
+})
 
 
 express.listen(3000);
